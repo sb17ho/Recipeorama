ab272aacf98741aebcbef46daf71cd4116be76c7
Simar Bhamra <sb17ho@brocku.ca>
2021-11-12 13:27:19 -0500

    Room Database Migration Handled

--- app/src/main/java/com/example/todo/data/Task.kt
+++ app/src/main/java/com/example/todo/data/Task.kt
@@ -1,6 +1,7 @@
 package com.example.todo.data
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.todo.priorityClasses.Priority
@@ -13,5 +14,7 @@ data class Task(
     var id: Int = 0,
     var title: String,
     var description: String,
-    var priority: Priority
-): Parcelable
+    var priority: Priority,
+    @ColumnInfo(defaultValue = "0") var isArchived: Int = 0,
+    @ColumnInfo(defaultValue = "0") var isTrash: Int = 0
+) : Parcelable
--- app/src/main/java/com/example/todo/data/TaskDatabase.kt
+++ app/src/main/java/com/example/todo/data/TaskDatabase.kt
@@ -5,8 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Task::class], version = 1, exportSchema = false)
+@Database(entities = [Task::class], version = 2, exportSchema = false)
 @TypeConverters(Converter::class)
 abstract class TaskDatabase : RoomDatabase() {
     abstract fun taskDao(): TaskDao
@@ -26,9 +28,16 @@ abstract class TaskDatabase : RoomDatabase() {
                     context.applicationContext,
                     TaskDatabase::class.java,
                     "task_database"
-                ).build()
+                ).addMigrations(migration_1_2).build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+
+        private val migration_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE todo_task ADD COLUMN isArchived INTEGER NOT NULL DEFAULT 0")
+                database.execSQL("ALTER TABLE todo_task ADD COLUMN isTrash INTEGER NOT NULL DEFAULT 0")
             }
         }
     }
